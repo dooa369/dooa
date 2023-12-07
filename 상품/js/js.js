b56501053d634554d3a02997e9446aaa938314cd
dooa369 <102269814+dooa369@users.noreply.github.com>
2023-12-07 11:36:03 +0900

    Add files via upload

--- 상품/js/js.js
+++ 상품/js/js.js
@@ -1,16 +1,8 @@
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
-
-loginButton.addEventListener("click", (event) => {
-    event.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-
-    if (username === "홍길동" && password === "1234") {
-        location.reload();
-        location.href ="로그인 된 홈페이지.html";
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-});  
+function printName()  {
+const in1 = document.getElementById('in1').value;
+const in3 = document.getElementById('in3').value;
+const in2 = document.getElementById('in2').value;
+const in4 = document.getElementById('in4').value;
+const in5 = document.getElementById('in5').value;
+document.getElementById("test").innerText = in1,in2,in3,in4,in5;
+}
